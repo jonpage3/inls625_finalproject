6750a36d6848bf3a325cf3267041e58ad7335690
Jon <jonpage3@gmail.com>
2018-11-28 22:39:59 -0500

    added thing to calculate the rates

--- csv_reader.py
+++ csv_reader.py
@@ -3,9 +3,6 @@ __author__ = 'Jonathon Page, jepage@email.unc.edu, Onyen = jepage'
 import csv
 import glob
 
-
-
-
 files = glob.glob('data/*.csv')
 acc = 2008
 for file in files:
@@ -15,8 +12,8 @@ for file in files:
            'E': ['American History', 0, 0], 'F': ['Local American History', 0, 0],
            'G': ['Geography, Anth, Recreation', 0, 0], 'H': ['Social Sciences', 0, 0],
            'J': ['Poli Sci', 0, 0], 'K': ['Law', 0, 0], 'L': ['Education', 0, 0],
-           'M': ['Music', 0, 0], 'N': ['Fine Arts', 0, 0], 'P': ['Language/Literature', 0, 0],
-           'Q': ['Science', 0, 0], 'R': ['Medicine', 0, 0], 'S': ['Agriculture', 0, 0],
+            'N': ['Fine Arts', 0, 0], 'P': ['Language/Literature', 0, 0],
+           'Q': ['Science', 0, 0], 'R': ['Medicine', 0, 0],
            'T': ['Technology', 0, 0], 'U': ['Military Sci', 0, 0], 'V': ['Naval Science', 0, 0],
            'Z': ['Library Science', 0, 0]}
     #fixed encoding problem
@@ -34,10 +31,16 @@ for file in files:
                 LCC[key][1] += 1
                 total_use = int(davis_data[row][1]) + int(davis_data[row][2])
                 LCC[key][2] += total_use
+    #add rate
+    for key in LCC:
+        try:
+            LCC[key].append(LCC[key][1]/LCC[key][2])
+        except ZeroDivisionError:
+            LCC[key].append(0)
     namefile = 'davis_results_' + str(acc) + '.csv'
     output_file = open(namefile,'w',newline='')
     outputWriter = csv.writer(output_file)
-    outputWriter.writerow(['Subject','Total Items','Total Usage'])
+    outputWriter.writerow(['Subject','Total Items','Total Usage','Items/Usage Rate'])
     for key in LCC:
         outputWriter.writerow(LCC[key])
     output_file.close()
